12f08f6b05aa89930accd20a8621c1fb62d2da05
Erik Tatsuya <erik.tatsuya@gmail.com>
2026-02-24 20:19:17 -0300

    commiting main.py

--- organize/gui/main.py
+++ organize/gui/main.py
@@ -3,13 +3,16 @@ from PySide6.QtCore import QObject, Slot
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWebChannel import QWebChannel
-
+from PySide6.QtWidgets import QFileDialog
+from organize.core import organize, CATEGORIES
 
 class Backend(QObject):
 
-    @Slot(int, int, result=int)
-    def select_folder_path(self, ):
-        return 
+    @Slot()
+    def select_folder_path(self):
+        directory = QFileDialog.getExistingDirectory(self, "Selecionar pasta")
+        if directory:
+            organize(directory, CATEGORIES)
 
 
 app = QApplication(sys.argv)
